21713e6ee251da12cda948a926c3a673ccaa1447
neetu3412 <neeth.yadav14@gmail.com>
2025-07-29 18:08:10 +0530

    fix: add missing requirements.txt to repo

--- app.py
+++ app.py
@@ -54,4 +54,7 @@ def get_data():
 # Run the Flask app on localhost:5000
 # -----------------------------
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+# modify the backend code for deploy on the google could so that streamlit can get it easly
